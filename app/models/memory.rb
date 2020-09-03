6949400a037857047913a444ddf4cae345808559
leighad <cdversiontwo@gmail.com>
2020-09-02 22:57:41 -0700

    add associations and validations to models

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,3 +1,4 @@
 class Memory < ApplicationRecord
   belongs_to :pet
+  validates :title, :description, presence: true
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ApplicationRecord
+    has_many :memories
+    validates :name, :bio, presence: true
 end
