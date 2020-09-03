6d1d32a69d63737fa6c02fed8e8f735b6eb4e7c0
leighad <cdversiontwo@gmail.com>
2020-09-02 23:18:05 -0700

    set up api v1 namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :memories
-  resources :pets
+  namespace :api do
+    namespace :v1 do 
+      resources :pets do
+        resources :memories
+      end
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
